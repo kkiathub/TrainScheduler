1d58e1fef31d2624541176d62da1d529d37db61b
J Frost <icefang@hotmail.com>
2019-06-09 01:16:20 -0700

    update name , destination

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,3 +1,10 @@
+const UPDATE_NONE = 0;
+const UPDATE_NAME = 1;
+const UPDATE_DESTINATION = 2;
+
+var updateKey;
+var updateType=UPDATE_NONE;
+
 // Initialize firebase
 var firebaseConfig = {
     apiKey: "AIzaSyDTFAcOCVCedrAgQLSkmKJy9_WDKoFX0Fg",
@@ -30,7 +37,8 @@ $("#add-train-btn").on("click", function (event) {
         freq: tranFreq
     };
     console.log(newTrain);
-    database.ref().push(newTrain);
+    var ikey = database.ref().push(newTrain).key;
+    console.log(ikey);
 
     // Clears all of the text-boxes
     $("#name-input").val("");
@@ -39,29 +47,43 @@ $("#add-train-btn").on("click", function (event) {
     $("#frequency-input").val("");
 });
 
-database.ref().on("child_added", function (childSnapshot) {
-    console.log(childSnapshot.val());
+function removeRecord() {
+    var trElem = $(this).parent().parent();
+    var keyid = trElem.attr("id");
+    console.log(keyid);
+    database.ref(keyid).remove();
+    trElem.remove();
+}
 
+database.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot.key);
+ 
     // Store everything into a variable.
     var trainName = childSnapshot.val().name;
     var trainDestination = childSnapshot.val().destination;
     var trainFirst = childSnapshot.val().first;
     var trainFreq = childSnapshot.val().freq;
-    console.log(moment.unix(trainFirst).format("HH:mm"));
     var minDiff = moment().diff(moment(trainFirst, "X"), "minutes");
     var minToArr = trainFreq - (minDiff % trainFreq);
 
     var nextArrTime = moment().add( minToArr, "minutes").format("HH:mm");
+    var btnElem = $("<button>");
+    btnElem.attr("type", "button");
+    btnElem.addClass("btn btn-secondary");
+    btnElem.text("remove");
 
-    console.log("min : " + minDiff);
+    
     // Create the new row
     var newRow = $("<tr>").append(
-        $("<td>").text(trainName),
-        $("<td>").text(trainDestination),
+        $("<td>").addClass("train-name").text(trainName),
+        $("<td>").addClass("train-dest").text(trainDestination),
         $("<td>").text(trainFreq),
         $("<td>").text(nextArrTime),
-        $("<td>").text(minToArr)
+        $("<td>").text(minToArr),
+        $("<td>").append(btnElem)
     );
+
+    newRow.attr("id", childSnapshot.key);
 
     // Append the new row to the table
     $("tbody").append(newRow);
@@ -89,4 +111,68 @@ function minuteUpdate() {
     return;
 }
 
+$("#btn-yes").on("click", function() {
+    var rowData = $("tbody").children();
+    var rowSelected=-1;
+
+    for(var i=0; i<rowData.length; i++) {
+        if ($(rowData[i]).attr("id")===updateKey) {
+            rowSelected = i;
+            break;
+        }
+    }
+
+    if (rowSelected>=0) {
+        var newVal = $("#text-new").val();
+        if (updateType==UPDATE_NAME) {
+            var updateElem = $($(rowData[rowSelected]).children()[0]);
+            updateElem.text(newVal);
+            database.ref(updateKey).update({ name: newVal });
+
+        } else if (updateType==UPDATE_DESTINATION) {
+            var updateElem = $($(rowData[rowSelected]).children()[1]);
+            updateElem.text(newVal);
+            database.ref(updateKey).update({ destination: newVal });
+        }
+    }
+
+    updateType = UPDATE_NONE;
+    $("#myModal").modal("hide");
+    $("#text-new").val("");
+});
+
+$("#btn-no").on("click", function() {
+    $("#myModal").modal("hide");
+    $("#text-new").val("");
+})
+
+$("#myModal").on('shown.bs.modal', function () {
+    $("#text-new").focus();
+});
+
+function updateName() {
+    var name = $(this).text();
+    updateKey = $(this).parent().attr("id");
+    updateType = UPDATE_NAME;
+    
+    $("#text-update").text("Update train name?");
+    $("#text-current").text(name);
+    $('#myModal').modal({backdrop: "static", keyboard: false});
+}
+
+function updateDestination() {
+    var name = $(this).text();
+    updateKey = $(this).parent().attr("id");
+    updateType = UPDATE_DESTINATION;
+
+    $("#text-update").text("Update destination?");
+    $("#text-current").text(name);
+    $('#myModal').modal({backdrop: "static", keyboard: false});
+}
+
+
 timeHandle = setInterval(minuteUpdate, 60000);
+
+$(document).on("click", ".btn-secondary", removeRecord);
+$(document).on("click", ".train-name", updateName);
+$(document).on("click", ".train-dest", updateDestination);
